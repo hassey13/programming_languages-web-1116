c398688d651d48700bea03191a8d489cbaae1ed9
Eric Hasselbring <ehasselbring@gmail.com>
2016-11-24 17:32:27 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,48 @@
 def reformat_languages(languages)
-  # your code here
+ # your code here
+ language_hash = {}
+ languages.each do |style, list|
+      list.each do |language, type_obj|
+            language = language.to_sym
+           if !language_hash[language]
+              language_hash[language] = type_obj
+              language_hash[language][:style] = [style]
+           else
+              language_hash[language][:style] << style
+           end
+      end
+ end
+ return language_hash
 end
+
+# {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
+#   :functional => {
+#     :clojure => {
+#       :type => "compiled"
+#     },
+#     :erlang => {
+#       :type => "compiled"
+#     },
+#     :scala => {
+#       :type => "compiled"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     }
+#
+#   }
+# }
